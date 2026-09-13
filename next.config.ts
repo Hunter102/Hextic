f7205f159eb066d530e11bd50f92cd2547bc5981
Hunter102 <patriotpoulin@gmail.com>
2026-09-13 13:32:20 -0400

    build

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.discordapp.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.shopify.com",
-      },
     ],
   },
 };
